227c07d6a172b6260e23bc961ae133e4a80d3b2f
jakyshan1990 <jakyshan1990@yahoo.com>
2024-11-24 22:58:32 +0200

    all

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+const addButton=document.getElementById("add-task-btn");
+const taskInput=document.getElementById("task-input");
+const taskList=document.getElementById("task-list");
+
+
+function addTask(){
+const taskText=taskInput.value.trim();
+if(taskText.length===0){
+    alert("please enter a task");
+}else{
+    const list=document.createElement("li");
+     list.textContent=taskText;
+     const rmvebtn=document.createElement("button");
+     rmvebtn.textContent="Remove";
+     rmvebtn.className='remove-btn';
+     rmvebtn.onclick=function(){ 
+        list.remove();
+     };
+list.append(rmvebtn);
+taskList.append(list);
+}
+
+
+}
+
+
+})
