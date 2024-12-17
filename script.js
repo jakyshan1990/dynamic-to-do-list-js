ed0c599fb84adec0fb54261195435eebe3b63386
jakyshan1990 <jakyshan1990@yahoo.com>
2024-12-17 22:26:50 +0200

    a33

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded",()=>{
-loadTasks();
+
 const addButton=document.getElementById("add-task-btn");
 const taskInput=document.getElementById("task-input");
 const taskList=document.getElementById("task-list");
@@ -9,28 +9,34 @@ const tasks=[];
 
 function loadTasks(){
 const storedTasks=JSON.parse(localStorage.getItem('tasks')||'[]');
-storedTasks.forEach(taskText => addTask(false));
+storedTasks.forEach(taskText => addTask(taskText,false));
 }
 
-function addTask(save=true){
-const taskText=taskInput.value;
-if(taskText.length===0){
-    alert("please enter a task");
-}else{
+function addTask(taskText="",save=true){
+//const taskText=taskInput.value;
+
+if(!taskText){taskText=taskInput.value;
+    if (!taskText) {
+  alert ('please enter a task');return;
+}
+
+}
+
+
+
     const list=document.createElement("li");
      list.textContent=taskText;
      const rmvebtn=document.createElement("button");
      rmvebtn.textContent="Remove";
      rmvebtn.classList.add('remove-btn');
-                rmvebtn.onclick=function(){ 
-                    list.remove();
-                };
+                rmvebtn.onclick=()=>list.remove();
+                
      list.appendChild (rmvebtn);
      taskList.appendChild (list);
      tasks.push(list);
      //localtasks=JSON.stringify(tasks);
      //localStorage.setItem(localtasks);
-}
+
 taskInput.value="";
 if (save) {
   const storedTasks = JSON.parse (localStorage.getItem ('tasks') || '[]');
@@ -42,14 +48,16 @@ if (save) {
 
 
 
-addButton.addEventListener("click",addTask(true));
+addButton.addEventListener("click",addTask);
 taskInput.addEventListener("keypress",(event)=>{let key1 =event.key;
-    if(key1=="Enter"){addTask(true);}
+    if(key1=="Enter"){addTask();}
 });
-addTask(false);
+addTask();
+loadTasks ();
+
 
 
 //tasks=JSON.parse( localStorage.getItem(localtasks) );
 //console.log(tasks);
 
-})
+});
